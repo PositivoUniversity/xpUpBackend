f7f366f909462707309798448905b6e6f3f7b19f
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2023-11-02 14:40:43 -0300

    criando put de dtosUser

---
+++ xpUpBackend/Dto/EditUsersDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace xpUpBackend.Dto
+{
+    public class EditUsersDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? PasswordTip { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? PasswordTipHash { get; set; }
+        public int? Role { get; set; }
+        public int Course { get; set; }
+    }
+}
