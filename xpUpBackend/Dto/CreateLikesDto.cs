4f3e528d4caf54101c9ad09a41fe582ec201220e
W3LLCR0S <wellcros.volkov@gmail.com>
2023-11-03 17:20:37 -0300

    Criação dos DTOs de likes e checkins.
    Aviso: pode conter alguns errinhos pois foi enviado meio rapido devido a minha internet ter caido.

---
+++ xpUpBackend/Dto/CreateLikesDto.cs
@@ -0,0 +1,11 @@
+﻿namespace xpUpBackend.Dto
+{
+    public class CreateLikesDto
+    {
+        public int Id { get; set; } 
+        public bool Like { get; set; }
+        public int LikedByUserId { get; set; } // ID do usuário que deu o like
+        public int NoticeId { get; set; } // ID da notícia relacionada
+        public int EventId { get; set; } // ID do evento relacionado
+    }
+}
