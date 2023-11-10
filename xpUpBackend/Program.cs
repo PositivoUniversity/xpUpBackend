2edd0e5f653c7f5c5ccba8e931fa9970ed573bd3
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2023-11-09 22:44:23 -0300

    config banco

--- xpUpBackend/Program.cs
+++ xpUpBackend/Program.cs
@@ -6,14 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<XpUpContext>(options => options.UseMySQL(connectionString));
+builder.Services.AddDbContext<XpUpContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin",
+        builder =>
+        {
+            builder.WithOrigins()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseCors("AllowAnyOrigin");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
